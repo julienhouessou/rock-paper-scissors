fde88f91f0f81b464551aea62982df3fb5d4f6cf
julienhouessou <houessoj@gmail.com>
2023-10-14 17:33:49 +0200

    Add final score recognition and message to announce winner

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 function playRound (playerSelection,computerSelection){
+
 const choices = ['Rock','Paper','Scissors']
 const random = Math.floor(Math.random() * choices.length);
 
@@ -15,7 +16,7 @@ function getComputerChoice (){
 playerSelection = prompt('Pick Rock, Paper, or Scissors','').toUpperCase();
 computerSelection = getComputerChoice().toUpperCase();
 
-if (playerSelection === computerSelection) {
+if (playerSelection == computerSelection) {
     message = 'Draw!';
     playerScore += 1;
     computerScore += 1;
@@ -25,24 +26,30 @@ else if (playerSelection == 'PAPER') {
         message = 'You Win! Paper beats Rock'
         playerScore += 1;
     }
-    else{ message = 'You Lose! Scissors beats Paper';
-    computerScore += 1;}
+    else{ 
+        message = 'You Lose! Scissors beats Paper';
+        computerScore += 1;
+        }
 }
 else if (playerSelection == 'ROCK') {
     if (computerSelection == 'PAPER'){
         message ='You Lose! Paper beats Rock';
         computerScore += 1;
     }
-    else{ message = 'You Win! Rock beats Scissors';
-            playerScore += 1;}
+    else{ 
+        message = 'You Win! Rock beats Scissors';
+        playerScore += 1;
+        }
 }
 else if (playerSelection == 'SCISSORS') {
     if (computerSelection == 'ROCK'){
         message ='You Lose! Rock beats Scissors';
         computerScore += 1;
     }
-    else{ message = 'You Win! Scissors beats Paper';
-    playerScore += 1;}
+    else{ 
+    message = 'You Win! Scissors beats Paper';
+    playerScore += 1;
+    }
 }
 else {message = 'Pick a valid option'}
 ;
@@ -51,12 +58,17 @@ console.log(`Computer chose: ${computerSelection}`);
 console.log(message);
 console.log(`Player score: ${playerScore}`);
 console.log(`Computer score: ${computerScore}`);
-return message
+return playerScore, computerScore;
+
 };
 
 function game (playRound){
     for (let i = 1; i <= 5; i++){
     playRound(playerSelection,computerSelection);
-}}
+};
+if (playerScore > computerScore) {console.log('The player wins!!!')}
+else if (playerScore < computerScore) {console.log('You lost to the computer...')}
+else {console.log('It\'s a draw, meh')}
+}
 
-game(playRound)
+game(playRound);
