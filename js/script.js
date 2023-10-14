1fb08cc9e5a32aa73e0508c4c89f77ac190afd56
julienhouessou <houessoj@gmail.com>
2023-10-14 15:52:23 +0200

    create logic for one round rock paper scissors

--- js/script.js
+++ js/script.js
@@ -4,3 +4,35 @@ const random = Math.floor(Math.random() * choices.length);
 function getComputerChoice (){
     return choices [random]
 };
+
+let playerSelection = prompt('Pick Rock, Paper, or Scissors').toUpperCase();
+let computerSelection = getComputerChoice;
+
+console.log(playerSelection);
+console.log(computerSelection());
+let message;
+
+if (playerSelection.toUpperCase() == computerSelection().toUpperCase()) {
+    message = 'Draw!'
+}
+else if (playerSelection == 'PAPER') {
+    if (computerSelection() == 'Rock'){
+        message = 'You Win! Paper beats Rock'
+    }
+    else{ message = 'You Lose! Scissors beats Paper'}
+}
+else if (playerSelection == 'ROCK') {
+    if (computerSelection() == 'Paper'){
+        message ='You Lose! Paper beats Rock'
+    }
+    else{ message = 'You Win! Rock beats Scissors'}
+}
+else if (playerSelection == 'SCISSORS') {
+    if (computerSelection() == 'Rock'){
+        message ='You Lose! Rock beats Scissors'
+    }
+    else{ message = 'You Win! Scissors beats Paper'}
+}
+else {message = 'Pick a valid option'}
+;
+console.log(message)
