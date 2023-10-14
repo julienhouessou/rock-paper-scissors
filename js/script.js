7128832fa602d25158618c18d1875e01f2c2bf60
julienhouessou <houessoj@gmail.com>
2023-10-14 16:03:05 +0200

    Create function playRound that simulates one round against the computer

--- js/script.js
+++ js/script.js
@@ -6,29 +6,30 @@ function getComputerChoice (){
 };
 
 let playerSelection = prompt('Pick Rock, Paper, or Scissors').toUpperCase();
-let computerSelection = getComputerChoice;
+let computerSelection = getComputerChoice();
 
 console.log(playerSelection);
-console.log(computerSelection());
+console.log(computerSelection);
 let message;
 
-if (playerSelection.toUpperCase() == computerSelection().toUpperCase()) {
+function playRound (playerSelection,computerSelection){
+if (playerSelection == computerSelection) {
     message = 'Draw!'
 }
 else if (playerSelection == 'PAPER') {
-    if (computerSelection() == 'Rock'){
+    if (computerSelection == 'Rock'){
         message = 'You Win! Paper beats Rock'
     }
     else{ message = 'You Lose! Scissors beats Paper'}
 }
 else if (playerSelection == 'ROCK') {
-    if (computerSelection() == 'Paper'){
+    if (computerSelection == 'Paper'){
         message ='You Lose! Paper beats Rock'
     }
     else{ message = 'You Win! Rock beats Scissors'}
 }
 else if (playerSelection == 'SCISSORS') {
-    if (computerSelection() == 'Rock'){
+    if (computerSelection == 'Rock'){
         message ='You Lose! Rock beats Scissors'
     }
     else{ message = 'You Win! Scissors beats Paper'}
@@ -36,3 +37,6 @@ else if (playerSelection == 'SCISSORS') {
 else {message = 'Pick a valid option'}
 ;
 console.log(message)
+};
+
+playRound(playerSelection,computerSelection)
